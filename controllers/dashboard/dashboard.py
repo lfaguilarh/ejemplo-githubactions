b9b6f2647f091bbb36e95ba4a3edd2989faf4d28
indra <indra@MacBook-Pro-de-indra.local>
2025-11-08 01:06:20 -0600

    se elimino todo

--- controllers/dashboard/dashboard.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, session
-
-dashboard = Blueprint('dashboard',__name__,template_folder='templates')
-
-@dashboard.route('/dashboard', methods = ['GET'])
-def index():
-    return render_template('/dashboard.html', user = session['user'])
